18199c5acea91870cc83333a633225a7b1c74bf3
djsowa <marc.sowa@gmail.com>
2018-04-25 18:06:42 +0200

    PlatformDeployment works fine

--- Deployment/Build&Release/Run.sh
+++ Deployment/Build&Release/Run.sh
@@ -5,16 +5,19 @@ echo "LOGIN....."
 cd ../../
 
 pwd
+ls
 
 cat './Deployment/Build&Release/app/pass.txt' |docker login --password-stdin -u eco-vadis  https://rancher-eco.westeurope.cloudapp.azure.com:8081
 
-sudo docker-compose -f platform-build-compose.yml build --pull
+sudo docker-compose -f platform-docker-compose.yml build --pull
 
-sudo docker-compose -f platform-build-compose.yml push 
+sudo docker-compose -f platform-docker-compose.yml push 
 
+
+##### PLEASE GENERATE ACCESS TOKEN via Rancher and put here to have acces rigth to rancher.
 export RANCHER_URL="https://rancher-eco.westeurope.cloudapp.azure.com:8080/" 
-export RANCHER_ACCESS_KEY="0F42178091E8930937D0" 
-export RANCHER_SECRET_KEY="MxbfTJsXkkJ6D35fUjXkCYJ2TtJ3cwQ4cCSbGzha" 
+export RANCHER_ACCESS_KEY="" 
+export RANCHER_SECRET_KEY="" 
 export RANCHER_ENVIRONMENT="dev" 
 
 echo "Env:"$RANCHER_ENVIRONMENT
