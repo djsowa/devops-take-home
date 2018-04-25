6ddf2bd2c1fb5dfc6c1de6046198d31e4b10e253
djsowa <marc.sowa@gmail.com>
2018-04-25 13:40:21 +0200

    Managment Server setup

---
+++ Deployment/Hosts/ManagmentHostSetup.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+sudo apt-get remove docker docker-engine docker.io
+sudo apt-get update
+sudo apt-get upgrade -Y
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common -y
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88
+
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+sudo apt-get update -y
+
+sudo apt-get install docker-ce=17.09.1~ce-0~ubuntu -y
+
+sudo apt-get install software-properties-common -y
+sudo add-apt-repository ppa:certbot/certbot -y
+sudo apt-get update -y
+sudo apt-get install certbot -y
