457a2577a05a7052fadca8d169d9aa415dfebdaf
djsowa <marc.sowa@gmail.com>
2018-04-25 19:08:04 +0200

    BuildScript update.5

--- Deployment/Build&Release/Run.sh
+++ Deployment/Build&Release/Run.sh
@@ -20,12 +20,8 @@ sudo docker build . -t eco-platform-release
 
 
 ##### PLEASE GENERATE ACCESS TOKEN via Rancher and put here to have acces rigth to rancher.
-sudo export RANCHER_URL="https://rancher-eco.westeurope.cloudapp.azure.com:8080/" &
-sudo export RANCHER_ACCESS_KEY="" & 
-sudo export RANCHER_SECRET_KEY="" &
-sudo export RANCHER_ENVIRONMENT="dev" 
 
-sudo docker run -e RANCHER_URL \
-                -e RANCHER_ACCESS_KEY \
-                -e RANCHER_SECRET_KEY \
-                -e RANCHER_ENVIRONMENT --rm eco-platform-release
+sudo docker run -e RANCHER_URL="https://rancher-eco.westeurope.cloudapp.azure.com:8080/" \
+                -e RANCHER_ACCESS_KEY="" \
+                -e RANCHER_SECRET_KEY="" \
+                -e RANCHER_ENVIRONMENT=dev --rm eco-platform-release
